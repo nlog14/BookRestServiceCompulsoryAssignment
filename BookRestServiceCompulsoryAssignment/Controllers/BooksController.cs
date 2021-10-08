c18318fe50560da917cc553af0e91b28bbd4dbe9
Nohely <ngedeon14@yahoo.com>
2021-10-08 20:12:12 +0200

    Add project files.

---
+++ BookRestServiceCompulsoryAssignment/Controllers/BooksController.cs
@@ -0,0 +1,110 @@
+﻿using BookLibraryCompulsoryAssignment;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using BookRestServiceCompulsoryAssignment.Managers;
+using Microsoft.AspNetCore.Http;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookRestServiceCompulsoryAssignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private BookManager _manager = new BookManager();
+        // GET: api/<BooksController>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetAllBooks([FromQuery] string isbn13)
+        {
+            var result = _manager.GetAllBooks();
+            if (result.Equals(null))
+            {
+                return NoContent();
+            }
+            else
+            {
+                return Ok(result);
+            }
+        }
+
+        // GET api/<BooksController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [HttpGet("{string}")]
+        public ActionResult<Book> GetWithISBN13(string isbn13)
+        {
+            Book result = _manager.GetBookByISBN13(isbn13);
+            if (result.Equals(null))
+            {
+                return NoContent();
+            }
+            else
+            {
+                return Ok(result);
+            }
+            
+        }
+
+        // POST api/<BooksController>
+        //POST adds a new item
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [HttpPost]
+        public ActionResult<Book>Post([FromBody] Book book)
+        {
+            Book newBook = _manager.AddNewBook(book);
+            if (newBook.Equals(null))
+            {
+                return Conflict();
+            }
+            else
+            {
+                return Created(newBook.ISBN13, newBook);
+            }
+            
+        }
+
+        // PUT api/<BooksController>/5
+        //PUT edits an existing item 
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [HttpPut("{string}")]
+        public ActionResult<Book> Put(string isbn13, [FromBody] Book book)
+        {
+            Book updatedBook = _manager.UpdateBook(isbn13, book);
+            if (updatedBook.Equals(null))
+            {
+                return NoContent();
+            }
+            else
+            {
+                return Ok(updatedBook);
+            }
+        }
+
+        // DELETE api/<BooksController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [HttpDelete("{string}")]
+        public ActionResult<Book> Delete(string isbn13)
+        {
+            Book deletedBook = _manager.DeleteBook(isbn13);
+            if (deletedBook.Equals(null))
+            {
+                return NoContent();
+            }
+            else
+            {
+                return Ok(deletedBook);
+            }
+
+        }
+    }
+}
---
+++ BookRestServiceCompulsoryAssignment/Managers/BookManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookLibraryCompulsoryAssignment;
+
+namespace BookRestServiceCompulsoryAssignment.Managers
+{
+    public class BookManager
+    {
+        public List<Book> ListOfBooks = new List<Book>()
+        {
+            new Book("Twilight", "Stephenie Meyer", 501, "9780316015844"),
+            new Book("To Kill a Mockingbird", "Harper Lee", 324, "9780446310789"),
+            new Book("Jinx", "Meg Cabot", 262, "9780060837648"),
+            new Book("Dune", "Frank Herbert", 688, "9780593099322")
+        };
+
+        public IEnumerable<Book> GetAllBooks(string isbn13 = "", string title ="", string author = "" )
+        {
+            isbn13 ??= string.Empty;
+            title ??= string.Empty;
+            author ??= string.Empty;
+            List<Book> AllBooks = new List<Book>(ListOfBooks);
+            var Results = AllBooks
+                .Where(book => book.ISBN13.Contains(isbn13, StringComparison.OrdinalIgnoreCase))
+                .Where(book => book.Title.Contains(title, StringComparison.OrdinalIgnoreCase))
+                .Where(book => book.Author.Contains(author, StringComparison.OrdinalIgnoreCase));
+            return Results;
+        }
+
+
+        public Book GetBookByISBN13(string isbn13)
+        {
+            Book ISBN13BookResult = ListOfBooks.Find(book => book.ISBN13.Equals(isbn13));
+            return ISBN13BookResult;
+        }
+
+        public Book AddNewBook(Book book)
+        {
+            ListOfBooks.Add(book);
+            return book;
+        }
+
+        public Book UpdateBook(string isbn13, Book update )
+        {
+            Book book = ListOfBooks.Find(book => book.ISBN13.Equals(isbn13));
+            book.Title = update.Title;
+            book.Author = update.Author;
+            book.NumberOfPages = update.NumberOfPages;
+            book.ISBN13 = update.ISBN13;
+            return book;
+
+        }
+
+        public Book DeleteBook(string isbn13)
+        {
+          var book = ListOfBooks.Find(book => book.ISBN13.Equals(isbn13));
+          ListOfBooks.Remove(book);
+          return book;
+        }
+
+
+
+
+    }
+}
